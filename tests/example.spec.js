252d54894058679adc8a01704af4f78f9d2e2923
Evellyn Thamires <xe.uyemura@gmail.com>
2023-12-11 19:55:10 -0300

    Adding login with success scenario

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,13 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('Login with success', async ({ page }) => {
+  await page.goto('https://automationpratice.com.br/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.locator('#user').click();
+  await page.locator('#user').fill('teste@gmail.com');
+  await page.locator('#password').click();
+  await page.locator('#password').fill('123456');
+  await page.getByRole('button', { name: 'login' }).click();
 });
